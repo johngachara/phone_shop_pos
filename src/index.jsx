4fe40363d630cee7dbef7f8fe6362559d4a35b10
johngachara <johngachara29@gmail.com>
2025-02-13 10:22:08 +0300

    add vercel speed insights and resolve firebase/app version conflicts

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,13 @@ import theme from '../src/components/theme'
 import { Provider } from 'react-redux'
 import store from 'components/redux/store.js'
 import { registerSW } from 'virtual:pwa-register'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <ChakraProvider theme={theme}>
+        <SpeedInsights />
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Provider store={store}>
             <App />
